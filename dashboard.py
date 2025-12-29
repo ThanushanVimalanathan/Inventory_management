ce7acc80c34a4bf920b1e1dd572a1c8247961438
VimalanathanThanushan <thanushaan69@gmail.com>
2025-12-29 23:54:36 +0530

    FEAT:Config scrollbar

--- dashboard.py
+++ dashboard.py
@@ -1,11 +1,103 @@
 from tkinter import *
+from tkinter import ttk
 from PIL import Image, ImageTk
 
+
+#----------------------------------Functionality Part--------------------------------------------------#
+def employee_form():
+   global back_btn_img
+   emp_frame = Frame(
+       window,
+       width=1070,
+       height=567,
+       bg='white'
+    ) 
+   emp_frame.place(x=200,y=100)
+   headingLabel = Label(emp_frame,text='Employee Details Controller', font=('times new roman',16,'bold'),bg="#060647",fg='white')
+   headingLabel.place(x=0,y=0,relwidth=1)
+   
+   back = Image.open('assets/back_button.png')
+   back = back.resize((34, 34), Image.LANCZOS)
+   back_btn_img = ImageTk.PhotoImage(back)
+   
+   back_button = Button(
+       emp_frame,
+       image=back_btn_img,
+       bd=0,
+       cursor='hand2',
+       bg='white',
+       command=lambda:emp_frame.place_forget()
+    )
+   back_button.place(x=10,y=30)
+   
+   topFrame = Frame(emp_frame,bg='white')
+   topFrame.place(x=0,y=65,relwidth=1,height=235)
+   search_frame = Frame(topFrame,bg='white')
+   search_frame.pack()
+   
+   search_combobox = ttk.Combobox(search_frame,values=('Id','Name','Email'),font=('times new roman',12),state='readonly')
+   search_combobox.set('Search by')
+   search_combobox.grid(row=0,column=0,padx=20)
+   
+   search_entry = Entry(search_frame,font=('times new roman',12),bg='lightyellow')
+   search_entry.grid(row=0,column=1)
+   
+   search_button = Button(search_frame,text='Search',font=('times new roman',12),width=10,cursor='hand2',fg='white',bg='#0f4d7d')
+   search_button.grid(row=0,column=2,padx=20)
+   
+   showAll_button = Button(search_frame,text='Show All',font=('times new roman',12),width=10,cursor='hand2',fg='white',bg='#0f4d7d')
+   showAll_button.grid(row=0,column=3)
+   
+   horizontal_scrollbar = Scrollbar(topFrame,orient=HORIZONTAL)
+   vertical_scrollbar = Scrollbar(topFrame,orient=VERTICAL)
+   employee_treeview = ttk.Treeview(topFrame,columns=('empid','name','email','gender','dob','contact','employement_type','education','work_shift','address','doj','salary','usertype'),show='headings',yscrollcommand=vertical_scrollbar.set,xscrollcommand=horizontal_scrollbar.set)
+   
+   horizontal_scrollbar.pack(side=BOTTOM,fill=X)
+   vertical_scrollbar.pack(side=RIGHT,fill=Y,pady=(10,0))
+   
+   horizontal_scrollbar.config(command=employee_treeview.xview)
+   vertical_scrollbar.config(command=employee_treeview.yview)
+   employee_treeview.pack(pady=(10,0))
+   
+   employee_treeview.heading('empid',text = 'EmpID')
+   employee_treeview.heading('name',text = 'Name')
+   employee_treeview.heading('email',text = 'E-Mail')
+   employee_treeview.heading('gender',text = 'Gender')
+   employee_treeview.heading('dob',text = 'Date Of Birth')
+   employee_treeview.heading('contact',text = 'Contact')
+   employee_treeview.heading('employement_type',text = 'Employee_Type')
+   employee_treeview.heading('education',text = 'Education')
+   employee_treeview.heading('work_shift',text = 'Work Shift')
+   employee_treeview.heading('address',text = 'Emp_Address')
+   employee_treeview.heading('doj',text = 'Date of Join')
+   employee_treeview.heading('salary',text = 'Salary')
+   employee_treeview.heading('usertype',text = 'user Type')
+   
+   employee_treeview.column('empid',width=60)
+   employee_treeview.column('name',width=140)
+   employee_treeview.column('email',width=180)
+   employee_treeview.column('gender',width=80)
+   employee_treeview.column('dob',width=100)
+   employee_treeview.column('contact',width=100)
+   employee_treeview.column('employement_type',width=120)
+   employee_treeview.column('education',width=120)
+   employee_treeview.column('work_shift',width=100)
+   employee_treeview.column('address',width=100)
+   employee_treeview.column('doj',width=100)
+   employee_treeview.column('salary',width=140)
+   employee_treeview.column('usertype',width=120)
+   
+
+
+
+
+#------------------------------------GUI Part-----------------------------------------------------------#
+#-------------------------------------------------------------------------------------------------------#
 window = Tk()
 window.title('Dashboard')
 window.geometry('1270x668+0+0')
 window.resizable(0,0)
-window.config(bg='white')
+window.config(bg='gray')
 
 img = Image.open('assets/inventory.png')
 img = img.resize((64, 64), Image.LANCZOS)
@@ -40,7 +132,7 @@ subtitleLabel.place(x=0,y=70,relwidth=1)
 
 #Create Sidebar
 leftFrame = Frame(window)
-leftFrame.place(x=10,y=122,width=200,height=500)
+leftFrame.place(x=0,y=100,width=200,height=500)
 
 
 LogoImg = Image.open('assets/checklist.png')
@@ -72,7 +164,9 @@ empoloyee_button = Button(
     text='Employees',
     font=('times new roman',20,'bold'),
     anchor='w',
-    padx=10
+    padx=10,
+    command=employee_form
+    
 )
 empoloyee_button.pack(fill=X)
 
